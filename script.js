2e418a770dbbcfa528821f53b3c47fb3e918585d
Matthies Kallsen <Matthies15@web.de>
2022-03-24 19:17:35 +0100

    added  captcha to contact form

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 //alert('This website is still under construction')
-if(window.location.href.includes('success')){
-    document.getElementById('emailsent').style.height = '60px';
-    document.getElementById('emailsent').style.visibility = 'visible'
-    document.getElementById('emailsent').style.opacity = '100%';
+function handleEmailSent() {
+    if (window.location.href.includes('success')) {
+        document.getElementById('emailsent').style.height = '60px';
+        document.getElementById('emailsent').style.visibility = 'visible'
+        document.getElementById('emailsent').style.opacity = '100%';
 
-    setTimeout(
-        function(){
-        document.getElementById('emailsent').style.height = '0';
-        document.getElementById('emailsent').style.visibility = 'hidden'
-        document.getElementById('emailsent').style.opacity = '0';
-        }
-        , 5000
-    )
-    history.replaceState({},'Matthies Kallsen','http://kallsen.net')
+        setTimeout(
+            function () {
+                document.getElementById('emailsent').style.height = '0';
+                document.getElementById('emailsent').style.visibility = 'hidden'
+                document.getElementById('emailsent').style.opacity = '0';
+            }
+            , 5000
+        )
+        history.replaceState({}, 'Matthies Kallsen', 'http://kallsen.net')
+    }
 }
-
 
 function toggleMobileMenu(element){
     element.classList.toggle('open')
@@ -53,3 +54,26 @@ function changeHiddenProjectsStatus(button){
         button.innerText = 'Hide additional projects'
     }
 }
+let solCaptcha;
+function generateCaptcha(){
+    let capFirst = Math.floor(Math.random() * (101) + 1)
+    let capSecond = Math.floor(Math.random() * (101) + 1)
+    document.getElementById('form-captcha-answer').placeholder = 'Captcha: What is ' + capFirst + ' + ' + capSecond + '?';
+    solCaptcha = capFirst + capSecond;
+    console.log(solCaptcha)
+}
+
+function handleCaptcha(button){
+    if(solCaptcha == document.getElementById('form-captcha-answer').value){
+        button.name = 'submit';
+        button.type = 'submit'
+    }else{
+        document.getElementById('form-captcha-wrong').style.display = 'block';
+    }
+}
+
+function init(){
+    handleEmailSent();
+    generateCaptcha();
+}
+init()
